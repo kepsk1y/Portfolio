428a8709c7b42dd230790b28dd3945419d069b01
kepsk1y <vadimkondratuk5@gmail.com>
2022-04-07 13:07:44 +0300

    Added smooth modal animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,62 @@
 $(function() {
+    
+    // Modal
+    let modalCall = $("[data-modal]");
+    let modalClose = $("[data-close]")
+
+     modalCall.on("click", function(event) {
+        event.preventDefault();
+        
+        let $this = $(this);
+        let modalID = $this.data('modal');
+
+        $(modalID).addClass('show');
+        $("body").addClass('no-scroll');
+
+        setTimeout(function() {
+            $(modalID).find(".modal__dialog").css({
+                transform: "rotateX(0)"
+            });
+        }, 200);
+      }); 
+    
+     modalClose.on("click", function(event) {
+        event.preventDefault();
+        
+        let $this = $(this);
+        let modalParent = $this.parents('.modal');
+
+        modalParent.find(".modal__dialog").css({
+            transform: "rotateX(90deg)"
+            });
+
+        setTimeout(function() {
+            modalParent.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 200);  
+      });
+
+      $(".modal").on("click", function(event) {
+        
+        let $this = $(this);
+
+        $this.find(".modal__dialog").css({
+            transform: "rotateX(90deg)"
+            });
+
+        setTimeout(function() {
+              $this.removeClass('show');
+              $("body").removeClass('no-scroll');
+        }, 200);  
+      });
+
+      $(".modal__dialog").on("click", function(event) {
+        event.stopPropagation();
+      }); 
+    
+    });
+
+    // Filter
 
     let filter = $("[data-filter]");
 
@@ -23,8 +81,7 @@ $(function() {
             });
         }
 
-        
-    });
+
 
 
 
